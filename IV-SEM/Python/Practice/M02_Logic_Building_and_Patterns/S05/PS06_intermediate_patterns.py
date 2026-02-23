61e47770cde60cc55a62fb00ce00b3e56029e503
Koushik Reddy <2401100cse0079@cse.suh.edu.in>
2026-02-23 10:28:11 +0530

    commited

--- IV-SEM/Python/Practice/M02_Logic_Building_and_Patterns/S05/PS06_intermediate_patterns.py
+++ IV-SEM/Python/Practice/M02_Logic_Building_and_Patterns/S05/PS06_intermediate_patterns.py
@@ -32,11 +32,34 @@ for i in range(1,n+1):
     print(" "*(n-i)+"* "*i) 
 for i in range(n-1,0,-1):
     print(" "*(n-i)+"* "*i)
-'''
+
 # print a number pyramid pattern
 n = int(input())
 for i in range(1,n+1):
     print(" "*(n-i),end="")
     for j in range(1,i+1):
         print(j,end=" ")
+    print()
+
+
+# print x pattern
+n = int(input())
+for i in range(n):
+    for j in range(n):
+        if i == j or i+j == n-1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+'''
+# hollow pyramid pattern    
+n = int(input())
+for i in range(1,n+1):
+    for j in range(1,n-i+1):
+        print(" ", end="")
+    for j in range(1,2*i):
+        if j == 1 or j == 2*i-1 or i == n:
+            print("*", end="")
+        else:
+            print(" ", end="")
     print()
