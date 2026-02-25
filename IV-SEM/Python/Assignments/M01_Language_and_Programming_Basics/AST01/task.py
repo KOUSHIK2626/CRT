97b2bbc38d2c9ae0077a2b9f142c6d0729e69bfb
Koushik Reddy <2401100cse0079@cse.suh.edu.in>
2026-02-25 18:46:22 +0530

    commit

--- IV-SEM/Python/Assignments/M01_Language_and_Programming_Basics/AST01/task.py
+++ IV-SEM/Python/Assignments/M01_Language_and_Programming_Basics/AST01/task.py
@@ -23,15 +23,15 @@ Output:
 ## Submission Rules
 - Only `task.py` will be evaluated
 '''
-def ticket_price(age):
-    if age < 5:
-        return "Free"
-    elif 5 <= age <= 17:
+def Ticket_Pricing(n: int) -> int:
+    if n < 5:
+        return 0
+    elif 5 <= n <= 17:
         return 10
-    elif 18 <= age <= 64:
+    elif 18 <= n <= 64:
         return 20
     else:
         return 15
 if __name__ == "__main__":
       age = int(input())
-      print(ticket_price(age))
+      print(Ticket_Pricing(age))
