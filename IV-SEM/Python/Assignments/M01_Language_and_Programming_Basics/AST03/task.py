b9f2b522895ab99cdc7027cf1f6670334f1bc855
Koushik Reddy <2401100cse0079@cse.suh.edu.in>
2026-03-02 09:06:26 +0530

    commit

--- IV-SEM/Python/Assignments/M01_Language_and_Programming_Basics/AST03/task.py
+++ IV-SEM/Python/Assignments/M01_Language_and_Programming_Basics/AST03/task.py
@@ -1,35 +1,9 @@
-'''
-# Student Grade System 
-
-## Problem Statement
-
-Calculate and store student grades for various subjects.
-
-This project stores student grades for various subjects, calculates their average, and determines their final grade. The user can input subject marks, and the system calculates the overall result (pass/fail).
-
-Input: Student name and grades for multiple subjects(3).
-Output: Student’s average grade and status (pass/fail).
-
-### Example
-Input:
-Name: Pavan, Grades: 85, 90, 78
-
-Output:
-"Average grade: 84.33, Status: Pass"
-
-## Instructions
-1. Write your solution in `task.py`
-2. Do NOT modify `test_task.py`
-3. Run tests locally before pushing
-
-## Submission Rules
-- Only `task.py` will be evaluated
-
-'''
 def Student_Grade_System(name:str,n1: int,n2: int,n3: int) -> str:
-    average = (n1 + n2 + n3) / 3
-    status = "Pass" if average >= 40 else "Fail"
-    return f"Average grade: {average:.2f}, Status: {status}"
+    grades = [n1, n2, n3]
+    avg = int(sum(grades) * 100 / 3) / 100
+    status = "Pass" if all(grade >= 35 for grade in grades) else "fail"
+    return f"Average grade: {avg}, Status: {status}"
+    
 
 if __name__ == '__main__':
     name = input()
--- IV-SEM/Python/Assignments/M01_Language_and_Programming_Basics/AST05/task.py
+++ IV-SEM/Python/Assignments/M01_Language_and_Programming_Basics/AST05/task.py
@@ -1,36 +1,14 @@
-'''
-# Reverse a String Using Loop
-
-## Problem Statement
-Reverse a string without using slicing.
-
-### Example
-Input: "Python"
-Output: "nohtyP"
-
-## Instructions
-1. Write your solution in `task.py`
-2. Do NOT modify `test_task.py`
-3. Run tests locally before pushing
-
-## Submission Rules
-- Only `task.py` will be evaluated
-
-'''
 from typing import List
-
-
 def Collatz_Sequence(n: int)-> List:
-      sequence = []
-      while n != 1:
-         sequence.append(n)
-         if n % 2 == 0:
-               n = n // 2
-         else:
-               n = 3 * n + 1
-      sequence.append(1)
-      return sequence
-      
+    sequence = [n]                  
+    while n != 1:                  
+        if n % 2 == 0:             
+            n = n // 2
+        else:                       
+            n = 3 * n + 1
+        sequence.append(n)         
+    return sequence
+   
 if __name__ == '__main__':
     n = int(input())
     print(Collatz_Sequence(n))
